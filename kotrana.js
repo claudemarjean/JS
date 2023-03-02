609a7619ec5eff689e03d8dc5b8dcb494198b9b7
claudemarjean <marjean.8888@gmail.com>
2023-03-02 23:17:29 +0100

    création de plusieurs objet et affichage

--- kotrana.js
+++ kotrana.js
@@ -9,11 +9,22 @@ class Episode {
     }
 }
 
-let episode = new Episode('strategy',90,false);
+let firstepisode = new Episode('strategy',21,true);
+let secondEpisode = new Episode('strategy',20,false);
+let thirdEpisode = new Episode('strategy',25,false);
   
   
   // =========================================
   
-  document.querySelector('#episode-info').innerText = `Episode: ${episode.title}
-  Duration: ${episode.duration} min
-  ${episode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+  document.querySelector('#episode-info-first').innerText = `Episode: ${firstepisode.title}
+  Duration: ${firstepisode.duration} min
+  ${firstepisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+
+  document.querySelector('#episode-info-second').innerText = `Episode: ${secondEpisode.title}
+  Duration: ${secondEpisode.duration} min
+  ${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+
+  document.querySelector('#episode-info-third').innerText = `Episode: ${thirdEpisode.title}
+  Duration: ${thirdEpisode.duration} min
+  ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+
