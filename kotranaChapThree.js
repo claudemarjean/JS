24960f394d2fd08123710aab9a621c94d4ffec0d
claudemarjean <marjean.8888@gmail.com>
2023-03-05 14:02:00 +0100

    ⬆️⬆️⬆️⬆️⬆️⬆️

---
+++ kotranaChapThree.js
@@ -0,0 +1,26 @@
+// Modify the code here
+// ======================
+
+const calculateAverageRating = (elementTableau) => {
+
+  let nblength = elementTableau.length;
+  let total = 0;
+  for(elem of elementTableau){
+    total += elem; 
+  }
+  return total/nblength;
+}
+
+// ======================
+
+const tauRatings = [5, 4, 5, 5, 1, 2];
+const colinRatings = [5, 5, 5, 4, 5];
+
+const tauAverage = calculateAverageRating(tauRatings);
+const colinAverage = calculateAverageRating(colinRatings);
+
+if (tauAverage && colinAverage) {
+  document.querySelector('#tau-score').innerText = tauAverage.toFixed(1) + ' stars';
+  document.querySelector('#colin-score').innerText = colinAverage.toFixed(1) + ' stars';
+  document.querySelector('#clara-score').innerText = `${calculateAverageRating([]) === 0 ? 'No ratings' : calculateAverageRating([]) + ' stars'}`
+}
