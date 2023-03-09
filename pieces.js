71df81cdd41d14c94f4b5f7faea9d31e5a6024fa
claudemarjean <marjean.8888@gmail.com>
2023-03-09 21:58:17 +0100

    filtre et trie

--- pieces.js
+++ pieces.js
@@ -1,19 +1,75 @@
-// Récuperation de pièces depuis la Json
-const reponse  = await fetch('pieces-autos.json');
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText =  article.categorie;
+for (let i = 0; i < pieces.length; i++) {
 
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    //Ajout des éléments au DOM pour l'exercice
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+
+ }
+
+ const boutonTrier = document.querySelector(".btn-trier");
+ 
+ boutonTrier.addEventListener("click",function(){
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function(a,b){
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+ })
+
+ const boutonFilter = document.querySelector(".btn-filtrer");
+
+ boutonFilter.addEventListener("click",function(){
+    const pieceFiltrees = pieces.filter(function(piece){
+        return piece.prix <= 35;
+    });
+    console.log(pieceFiltrees); 
+ })
+
+ const boutonAfficher = document.querySelector(".btn-afficher");
+
+ boutonAfficher.addEventListener("click",function(){
+    const pieceDescription = pieces.filter(function(piece){
+        return piece.description;
+    })
+    console.log(pieceDescription);
+ });
+
+const boutonTrierDecroissant = document.querySelector(".btn-trier-decroissant");
+
+boutonTrierDecroissant.addEventListener("click",function(){
+    const pieceOrdreDecroissant = Array.from(pieces);
+    pieceOrdreDecroissant.sort(function(a,b){
+        return b.prix - a.prix;
+    })
+    console.log(pieceOrdreDecroissant);
+});
